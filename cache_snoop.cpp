e1b8dbfd285440dca85257272d31517d46365b59
Sayam Sethi <sayam.sethi2@gmail.com>
2023-10-11 18:10:17 -0500

    initial commit

---
+++ cache_snoop.cpp
@@ -0,0 +1,19 @@
+// cache.c
+//   by Derek Chiou
+//      Oct. 8, 2023
+// 
+
+// for 382N-10
+
+// STUDENTS: YOU ARE EXPECTED TO MODIFY THIS FILE.
+
+#include <stdio.h>
+#include "types.h"
+#include "cache.h"
+#include "iu.h"
+#include "helpers.h"
+
+// ***** FYTD ***** 
+response_t cache_t::snoop(net_cmd_t net_cmd) {
+    
+}
